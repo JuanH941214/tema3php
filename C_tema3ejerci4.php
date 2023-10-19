7a5ff121a012b77899506db706857ff8674d717b
JuanH941214 <juanhernandez941214@gmail.com>
2023-10-19 09:45:41 +0200

    ejercicio 4

---
+++ C_tema3ejerci4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <h1>EJERCICIO 4 ARRAY</h1>   
+</body>
+</html>
+<?php
+$perfil= array("nombre"=>"juan", "edat"=>"28","email:"=>"juan@gmail.com", "menjar favorit"=>"pizza");
+var_dump($perfil);
+
+echo $perfil['nombre'];
+
+?>
+
